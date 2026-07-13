06a1a854380e4a5322d349a6a8f19e896c373493
Mohc Bebra <danilasniper66@gmail.com>
2026-07-13 23:59:09 +0500

    bebra

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -52,27 +52,49 @@ func triangulate_map():
 	result_vertices.append(pl1_position)
 	result_vertices.append(pl2_position)
 	result_vertices.append(Vector2.ZERO)
+	#result_vertices.append(Vector2(0., -map_size.y / 2))
+	#result_vertices.append(Vector2(0., map_size.y / 2))
+	#result_vertices.append(Vector2(-map_size.x / 2, 0.))
+	#result_vertices.append(Vector2(map_size.x / 2, 0.))
 	
 	var h_split_points: PackedVector2Array
 	var h_dist = map_size.x / h_split
 	for i: int in h_split + 1:
 		var new_point: Vector2 = Vector2(-map_size.x / 2 + h_dist * i, -map_size.y / 2)
 		h_split_points.append(new_point)
+		new_point = Vector2(-map_size.x / 2 + h_dist * i, map_size.y / 2)
+		h_split_points.append(new_point)
 	
 	var v_split_points: PackedVector2Array
 	var v_dist = map_size.y / v_split
-	for p: Vector2 in h_split_points:
-		for i: int in v_split + 1:
-			if i != 0:
-				var new_point: Vector2 = Vector2(p.x, p.y + v_dist * i)
-				v_split_points.append(new_point)
+	for i: int in v_split + 1:
+		var new_point: Vector2 = Vector2(-map_size.x / 2, -map_size.y / 2 + v_dist * i)
+		v_split_points.append(new_point)
+		new_point = Vector2(map_size.x / 2, -map_size.y / 2 + v_dist * i)
+		v_split_points.append(new_point)
+	
+	
 	
 	h_split_points.append_array(v_split_points)
 	for p: Vector2 in h_split_points:
 		if not result_vertices.has(p):
-			p += Vector2((randf()*2-1) * map_size.x / 16, (randf()*2-1) * map_size.y / 16)
-			p = Vector2(clampf(p.x, -map_size.x / 2, map_size.x / 2), clampf(p.y, -map_size.y / 2, map_size.y / 2))
 			result_vertices.append(p)
+	
+	var insides_points: PackedVector2Array
+	for i: int in h_split:
+		if i != 0:
+			for j: int in v_split:
+				if j != 0:
+					var new_point: Vector2 = Vector2(-map_size.x / 2 + h_dist * i, -map_size.y / 2 + v_dist * j)
+					insides_points.append(new_point)
+	
+	for p: Vector2 in insides_points:
+		if not result_vertices.has(p):
+			if randi_range(0, 2) == 0:
+				p += Vector2((randf()*2-1) * h_dist / 4, (randf()*2-1) * v_dist / 4)
+				p = Vector2(clampf(p.x, -map_size.x / 2, map_size.x / 2), clampf(p.y, -map_size.y / 2, map_size.y / 2))
+			result_vertices.append(p)
+	
 	
 	polygon_grid.polygon = result_vertices
 	polygon_grid.triangulate()
